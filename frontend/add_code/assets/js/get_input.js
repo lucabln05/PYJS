24f64e23bd475a86e8c1395b0e139b2dae1da90b
lucabln05 <luca.bellon@icloud.com>
2022-11-21 16:38:40 +0100

    add ^#^ at the and of one line

--- frontend/add_code/assets/js/get_input.js
+++ frontend/add_code/assets/js/get_input.js
@@ -1,8 +1,6 @@
 //this script read out the input fields and send the data to the server
 
 
-// if press button get input value
-
 const ws = new WebSocket('ws://localhost:10');  // connect to ws server 
 
 // if connectet send a message to the server, end refresh the connection
@@ -10,9 +8,13 @@ ws.addEventListener('open', () => {
     console.log('connected to server');
 });
 function getinput() {
+        // get the input value
         var username = document.getElementById("user").value;
         var CodeLanguage = document.getElementById("language").value;
         var Code = document.getElementById("code").value;
+
+        // get value form code input field and add a ^#^ to the end of a line
+        var Code = Code.replace(/\n/g, "^#^");
 
         // make the inputs server readable
         var message_server_readable = 'add_post' + '/--/' + username + '/--/' + CodeLanguage + '/--/' + Code;
